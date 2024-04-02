39cd9239081ab8ee1612a764ddcbd841d1d6dd93
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2024-04-02 21:34:42 +0000

    2.13.4 Добавил class transformer helpers из видео урока

--- project/libs/shared/helpers/src/lib/common.ts
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -1,1 +1,26 @@
-// TODO - Implement DTO helpers from Lecture 2 live demo
+import { ClassTransformOptions, plainToInstance } from 'class-transformer';
+
+type PlainObject = Record<string, unknown>;
+
+export function fillDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T;
+
+export function fillDto<T, V extends PlainObject[]>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T[];
+
+export function fillDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T | T[] {
+  return plainToInstance(DtoClass, plainObject, {
+      excludeExtraneousValues: true,
+      ...options,
+  });
+}
