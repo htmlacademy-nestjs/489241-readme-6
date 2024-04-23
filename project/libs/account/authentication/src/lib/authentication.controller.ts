144956de33dcf409e28eeea5e5be9768670c5d70
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2024-04-23 21:48:35 +0000

    5.5.14 Добавит поддержку JWT в `AuthenticationController`

--- project/libs/account/authentication/src/lib/authentication.controller.ts
+++ project/libs/account/authentication/src/lib/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Param, Post, UseGuards } from "@nestjs/common";
 import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { fillDto } from '@project/shared-helpers';
@@ -9,6 +9,8 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { AuthenticationErrors, AuthenticationOperationDescription, AuthenticationResponseMessage } from "./authentication.constants";
 import { UserRdo } from "./rdo/user.rdo";
+import { LoggedUserRdo } from "./rdo/logged-user.rdo";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -33,13 +35,14 @@ export class AuthenticationController {
   @ApiOperation({ summary: AuthenticationOperationDescription.Login })
   @ApiOkResponse({
     description: AuthenticationResponseMessage.LoggedSuccess,
-    type: UserRdo
+    type: LoggedUserRdo
   })
   @ApiResponse({ status: HttpStatus.FORBIDDEN, description: AuthenticationResponseMessage.LoggedError })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, description: AuthenticationErrors.UserNotFound })
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authenticationService.verifyUser(dto);
-    return fillDto(UserRdo, verifiedUser.toPOJO());
+    const userToken = await this.authenticationService.createUserToken(verifiedUser);
+    return fillDto(LoggedUserRdo, { ...verifiedUser.toPOJO(), ...userToken });
   }
 
   @Get(':id')
@@ -52,5 +55,11 @@ export class AuthenticationController {
   public async get(@Param('id', MongoIdValidationPipe) id: string) {
     const existUser = await this.authenticationService.getUser(id);
     return fillDto(UserRdo, existUser.toPOJO());
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/demo/:id')
+  public async demoPipe(@Param('id') id: number) {
+    console.log(typeof id);
   }
 }
